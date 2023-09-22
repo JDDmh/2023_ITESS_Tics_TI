fed2fa6d2e88e498d42b2d114f27ff3601ef43cd
JDDmh <ti20110357@salvatierra.tecnm.mx>
2023-09-22 15:30:57 -0600

    first commit

---
+++ src/pkg2023_itess/Main.java
@@ -0,0 +1,23 @@
+/*
+ITESS-TICS
+Taller de Investigación
+Intento de curso de certificacion java 0Z1-808
+by EJMp
+22/09/2023
+ */
+package pkg2023_itess;
+
+/**
+ *
+ * @author juand
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
